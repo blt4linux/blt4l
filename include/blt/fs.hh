21018635650c82812b25685075b854d0bad00639
Roman Hargrave <roman@hargrave.info>
2016-04-06 12:10:19 -0500

    Add the remaining worthwhile FS utilities

--- include/blt/fs.hh
+++ include/blt/fs.hh
@@ -7,6 +7,12 @@ namespace blt {
     namespace fs {
 
         std::vector<std::string> list_directory(std::string, bool = false);
+
+        std::string read_file(std::string);
+
+        bool path_is_dir(std::string);
+
+        bool create_directory(std::string);
     
     }
 }
--- src/fs.cc
+++ src/fs.cc
@@ -1,15 +1,26 @@
 #include <blt/fs.hh>
 #include <blt/log.hh>
+
 #include <dirent.h>
+#include <sys/stat.h>
+
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
 
 namespace blt {
     namespace fs {
 
         using std::string;
+        using std::stringstream;
         using std::vector;
-        
+        using std::ifstream;
+
+        /**
+         * Compute the contents of a directory and return them in vector
+         */
         vector<string>
         list_directory(string path, bool listDirs)
         {
@@ -37,6 +48,65 @@ namespace blt {
 
             return result;
         }
+
+        /**
+         * Read the contents of a file and return them
+         */
+        string
+        read_file(string path)
+        {
+            ifstream istream(path);
+            string body;
+
+            istream.seekg(0, std::ios::end);
+            body.reserve(istream.tellg());
+            istream.seekg(0, std::ios::beg);
+            body.assign(std::istreambuf_iterator<char>(istream), 
+                        std::istreambuf_iterator<char>());
+
+            return body;
+        }
+
+        /**
+         * Check if a path exists, and is a directory.
+         */
+        bool
+        path_is_dir(string path)
+        {
+            struct stat fileInfo;
+            
+            if (stat(path.c_str(), &fileInfo))
+            {
+                return S_ISDIR(fileInfo.st_mode);
+            }
+
+            return false;
+        }
+
+
+        /**
+         * Linux mkdirs() approximation
+         */
+        bool
+        create_directory(string path)
+        {
+            {
+                string seg;
+                stringstream pathBuilder;
+                stringstream pathStream(path);
+                while (std::getline(pathStream, seg, '/'))
+                {
+                    if (!seg.empty()) // should break on this instead of continuing...
+                    {
+                        pathBuilder << seg + '/';
+                        mkdir(pathBuilder.str().c_str(), 0655);
+                    }
+                }
+            }    
+
+            return true;
+        }
+
 
     }
 }
