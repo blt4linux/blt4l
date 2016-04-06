c3246ff6866bc185151b9286ab9cc5d6b3049136
Roman Hargrave <roman@hargrave.info>
2016-04-06 10:36:21 -0500

    Fix lua_pcall call

---
+++ src/lapi.cc
@@ -0,0 +1,64 @@
+#include <blt/lapi.hh>
+#include <blt/hook.hh>
+#include <string>
+#include <cstddef> // size_t
+
+namespace blt {
+    namespace lapi {
+        using std::string;
+
+        /*
+         * pcall impl
+         */
+
+        int
+        pcall(lua_state* state)
+        {
+            int args = lua_gettop(state);
+            int result = lua_pcall(state, args - 1, -1, 0);
+
+            if (result == LUAErrRun) 
+            {
+                // TODO Logging::...
+                return 0;
+            }
+
+            lua_pushboolean(state, result == 0);
+            lua_insert(state, 1);
+
+            return lua_gettop(state);
+        }
+
+        /*
+         * dofile impl
+         */
+
+        int
+        dofile(lua_state* state)
+        {
+            int args = lua_gettop(state);
+            size_t length = 0;
+            const char* filename = lua_tolstring(state, 1, &length);
+            int error = luaL_loadfile(state, filename);
+
+            if (error = LUAErrSyntax)
+            {
+                // TODO Logging::...
+            }
+
+            error = lua_pcall(state, 0, 0, 0);
+
+            if (error = LUAErrRun)
+            {
+                // TODO logging
+            }
+
+            return 0;
+        }
+
+        /*
+         * HTTP details moved to lapi_http.cc
+         */
+
+    }
+}
