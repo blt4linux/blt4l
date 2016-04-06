db6dfd5804c161482ba9d3f0dd4270e278bf0c51
Roman Hargrave <roman@hargrave.info>
2016-04-06 10:34:42 -0500

    Implement dohttpreq

---
+++ src/lapi_http.cc
@@ -0,0 +1,74 @@
+#include <blt/lapi.hh>
+#include <blt/hook.hh>
+#include <blt/http.hh>
+#include <cstddef> // size_t
+#include <string>
+
+namespace blt {
+    namespace lapi {
+        using std::string;
+
+        struct LUAHttpData {
+            int         funcRef;
+            int         progressRef;
+            int         reqIdent;
+            lua_state*  state;
+        };
+
+        static int HTTPRequestIdent = 0; // why?
+
+        /*
+         * HTTP Callback Region
+         */
+
+        static void
+        http_completion_cb(void* data, string contents)
+        {
+            LUAHttpData* httpData = (LUAHttpData*) data;
+
+        }
+
+        int
+        dohttpreq(lua_state* state)
+        {
+            // XXX logging
+            
+            int args = lua_gettop(state);
+            int progressRef = 0;
+
+            if (args >= 3)
+            {
+                progressRef = luaL_ref(state, LUARegistryIndex);
+            }
+
+            int functionRef = luaL_ref(state, LUARegistryIndex);
+            
+            string url;
+            {
+                size_t urlLen;
+                const char* urlCharArr = lua_tolstring(state, 1, &urlLen);
+                url = string(urlCharArr, urlLen);
+            }
+
+            LUAHttpData* httpData = new LUAHttpData();
+            {
+                httpData->funcRef = functionRef;
+                httpData->progressRef = progressRef;
+                httpData->state = state;
+                httpData->reqIdent = HTTPRequestIdent++;
+            }
+
+
+            HTTPItem* reqItem = new HTTPItem();
+            {
+                reqItem->callback   = http_completion_cb;
+                reqItem->data       = httpData;
+                reqItem->url        = url;
+            }
+
+            HTTPManager::get_instance()->launch_request(reqItem);
+            lua_pushinteger(state, HTTPRequestIdent);
+            return 1;
+        }
+    }
+}
