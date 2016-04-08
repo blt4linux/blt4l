fdd96f36a63c2f3085b81918559843c69c7891f2
Roman Hargrave <roman@hargrave.info>
2016-04-07 20:59:39 -0500

    Change the dl search target to a more unique symbol

--- src/blt_main.cc
+++ src/blt_main.cc
@@ -46,7 +46,7 @@ blt_main ()
      *
      * lua_call will do just fine in this case, as it's only present in payday
      */
-    if (dlHandle && dlsym(dlHandle, "lua_call")) {
+    if (dlHandle && dlsym(dlHandle, "_ZN3dsl12LuaInterface8newstateEbbNS0_10AllocationE")) {
         blt::blt_init_hooks(dlHandle);
     } else if(dlHandle) {
         cerr << "lua_call wasn't found in dlHandle, won't load!\n";
