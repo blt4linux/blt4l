4aa24bdefb9687f73c268a3131147a40d5edd67e
Roman Hargrave <roman@hargrave.info>
2017-09-08 17:01:40 -0500

    Afford some flexibility in package.sh

--- packaging/package.sh
+++ packaging/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCR_HOME=$(dirname $(readlink -f $0))
-REPO_HOME=$(realpath $SCR_HOME/..)
+REPO_HOME=${REPO_HOME:-$(realpath $SCR_HOME/..)}
 PKG_HOME=$REPO_HOME/packaging # in case SCR_HOME differs
 
 # For source release, check that we are in the correct folder
@@ -9,50 +9,9 @@ if [ ! -f $REPO_HOME/CMakeLists.txt ]; then
     echo "Can't find CMakeLists.txt, is install.sh in the correct folder?"
 fi
 
-# Dependency check
-MISSING_PKGS=""
-
-_test_lib() {
-    #ld hack. should work on steamos/debian/ubuntu. sacrifices best compatibility for shorter solution that works for >80% of users
-    # manual install is easy, too, and anyone that has a wierd ld should be able to do this themself
-    ld -l$1 2> /dev/null
-}
-
-_test_pkg() {
-    dpkg -s $@ 2>&1 > /dev/null
-}
-
-if ! _test_lib curl; then 
-    MISSING_PKGS="$MISSING_PKGS libcurl4-openssl-dev"
-fi
-
-if ! _test_lib z; then
-    MISSING_PKGS="$MISSING_PKGS zlib1g-dev"
-fi
-
-if ! which cmake 2>&1 > /dev/null; then
-    MISSING_PKGS="$MISSING_PKGS cmake"
-fi
-
-if which dpkg 2>&1 > /dev/null; then
-    if ! _test_pkg build-essential; then
-        MISSING_PKGS="$MISSING_PKGS build-essential"
-    fi
-fi
-
-if [ ${#MISSING_PKGS} -gt 1 ]; then
-    if which apt-get 2>&1 > /dev/null; then
-        echo Enter your password to install these packages: $MISSING_PKGS
-        sudo apt-get install $MISSING_PKGS
-    else
-        echo The following packages must be installed: $MISSING_PKGS
-        echo They may have a different name on your distribution
-    fi
-fi
-
 # Build release lib
 
-BUILD_DIR=$SCR_HOME/package_build
+BUILD_DIR=${BUILD_DIR:-$SCR_HOME/package_build}
 
 if [ ! -d $BUILD_DIR ]; then
     mkdir $BUILD_DIR
